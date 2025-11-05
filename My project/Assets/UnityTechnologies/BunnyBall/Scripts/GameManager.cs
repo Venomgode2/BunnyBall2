23f25b6817ce92081f1e5da9b9e811237ce77fb7
Veno <venomgode2@gmail.com>
2025-11-05 09:33:00 -0500

    little confused
    i tried to load level 1

--- My project/Assets/UnityTechnologies/BunnyBall/Scripts/GameManager.cs
+++ My project/Assets/UnityTechnologies/BunnyBall/Scripts/GameManager.cs
@@ -10,7 +10,8 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-       //is the game over?
+        //is the game over?
+        if (gameOver) { GameOver(); }
     }
 
     void GameOver() {
---
+++ My project/Assets/UnityTechnologies/BunnyBall/Scripts/LevelManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public void LoadLevel1()
+
+SceneManager.LoadScene(LoadLevel1().name);
